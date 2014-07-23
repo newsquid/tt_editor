eb07fabc436d12981059d5c9919e3247b6febcda
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2014-07-23 16:58:34 +0200

    Dont be so negative --- its a work in progress

--- src/scripts/components/scribe-plugin-tt-insert-image/scribe-plugin-tt-insert-image.js
+++ src/scripts/components/scribe-plugin-tt-insert-image/scribe-plugin-tt-insert-image.js
@@ -1,11 +1,36 @@
-define(function(){
+define('scribe-plugin-tt-insert-image', function(){
 
   'use strict';
 
   return function(){
-
     return function(scribe){
 
+        var TTInsertImageCommand = new scribe.api.Command("tt_insertImage");
+
+        TTInsertImageCommand.nodeName = 'IMG';
+
+        TTInsertImageCommand.execute = function() {
+            alert("Here's a story, all about how");
+            alert("my life got flipped, turned upside down");
+
+            if(this.queryState()) {
+                var selection = new scribe.api.Selection();
+                console.log(selection);
+            }
+
+            var imgLink = "http://gooel.com";
+
+            scribe.api.SimpleCommand.prototype.execute.call(this, imgLink);
+        };
+
+        TTInsertImageCommand.queryState = function() {
+           var selection = new scribe.api.Selection();
+           return !! selection.getContaining(function(node) {
+               return node.nodeName == this.nodeName;
+           }).bind(this);
+        };
+
+        scribe.commands.tt_insertImage = TTInsertImageCommand;
 
     };
   };
--- tt_editor.js
+++ tt_editor.js
@@ -5099,7 +5099,8 @@ define('scribe-plugin-toolbar',[],function () {
 
 });
 
-//# sourceMappingURL=scribe-plugin-toolbar.js.map;
+//# sourceMappingURL=scribe-plugin-toolbar.js.map
+;
 define('scribe-plugin-heading-command',[],function () {
 
   /**
@@ -7054,14 +7055,39 @@ define('scribe-plugin-sanitizer',[
 });
 
 //# sourceMappingURL=scribe-plugin-sanitizer.js.map;
-define('scribe-plugin-tt-insert-image',[],function(){
+define('scribe-plugin-tt-insert-image', [],function(){
 
   
 
   return function(){
-
     return function(scribe){
 
+        var TTInsertImageCommand = new scribe.api.Command("tt_insertImage");
+
+        TTInsertImageCommand.nodeName = 'IMG';
+
+        TTInsertImageCommand.execute = function() {
+            alert("Here's a story, all about how");
+            alert("my life got flipped, turned upside down");
+
+            if(this.queryState()) {
+                var selection = new scribe.api.Selection();
+                console.log(selection);
+            }
+
+            var imgLink = "http://gooel.com";
+
+            scribe.api.SimpleCommand.prototype.execute.call(this, imgLink);
+        };
+
+        TTInsertImageCommand.queryState = function() {
+           var selection = new scribe.api.Selection();
+           return !! selection.getContaining(function(node) {
+               return node.nodeName == this.nodeName;
+           }).bind(this);
+        };
+
+        scribe.commands.tt_insertImage = TTInsertImageCommand;
 
     };
   };
